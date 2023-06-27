ce0c7c568c6be1b786338cf164b4adaa3697397a
MALAQ\kmala <kmalak4822@gmail.com>
2023-06-27 23:00:53 +0200

    2_buttons_working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:kacapp/fadeTxt.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Kac App',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: ' Kac App'),
+      home: MyHomePage(title: ' Kac App'),
     );
   }
 }
@@ -23,55 +23,120 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  final String title; 
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool canChooseLvl = false;
+  final TextStyle aTxtStyle =
+      const TextStyle(color: Colors.white, fontSize: 16);
+  double lvl = 0.5;
+
+  final Uri url = Uri(scheme: 'tel', path: '999');
+
+  String shortenedLvl() {
+    double level = lvl * 10;
+    return level.toStringAsFixed(1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(Icons.loyalty, size: 58, color: Colors.white),
-              Text(widget.title,
-                  style:const TextStyle(color: Colors.white, fontSize: 34, fontWeight: FontWeight.bold)),
-            ],
-          )),
+        centerTitle: true,
+        toolbarHeight: 80,
+        leadingWidth: 80,
+        leading: Row(
+          children: [
+            const SizedBox(width: 20),
+            Image.asset('assets/cross.png', width: 50)
+          ],
+        ),
+        backgroundColor: const Color.fromRGBO(160, 20, 52, 1),
+        title: Text(widget.title,
+            style: const TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w500)),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            button('ZNAJDŹ PIJĄCEGO W POBLIŻU'),
-            button('ZAMÓW KARETKE'),
-            button('UDOSTĘPNIJ SWÓJ STAN UPOJENIA'),
-            button('ZAMÓW KLINA ON-LINE'),
+            const Spacer(),
+            button(Text('ZNAJDŹ PIJĄCEGO W POBLIŻU', style: aTxtStyle), () {}),
+            const Spacer(),
+            button(Text('ZAMÓW KARETKE', style: aTxtStyle), () async {
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url);
+              }
+            }),
+            const Spacer(),
+            button(
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Visibility(
+                      visible: !canChooseLvl,
+                      child: Text('UDOSTĘPNIJ SWÓJ STAN UPOJENIA',
+                          style: aTxtStyle)),
+                  Visibility(
+                      visible: canChooseLvl,
+                      child: Slider(
+                        value: lvl,
+                        onChanged: (newValue) {
+                          setState(() {
+                            lvl = newValue;
+                          });
+                        },
+                        activeColor: Colors.white,
+                        inactiveColor: Colors.white.withOpacity(0.5),
+                      )),
+                  Visibility(
+                      visible: canChooseLvl,
+                      child: Expanded(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                            Text(
+                              "'Upodliłem się ${shortenedLvl()}/10'",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            IconButton(
+                                onPressed: () async {
+                                  await Share.share(
+                                      'Upodliłem się ${shortenedLvl()}/10 ',
+                                      subject: 'Jak Bardzo Się Upodliłem');
+                                },
+                                icon: const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                ))
+                          ])))
+                ]), () {
+              setState(() {
+                canChooseLvl = !canChooseLvl;
+              });
+            }),
+            const Spacer(),
+            button(Text('ZAMÓW KLINA ON-LINE', style: aTxtStyle), () {}),
+            const Spacer(),
           ],
         ),
       ),
     );
   }
 
-  Widget button(String txt) => Column(children: [
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              fixedSize: Size(MediaQuery.of(context).size.width * 3 / 4, MediaQuery.of(context).size.width * 1 / 4),
-              backgroundColor: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: const BorderSide(width: 2),
-              ),
-            ),
-            onPressed: () {},
-            child: Text(
-              txt,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-            )),
-        const SizedBox(height: 40)
-      ]);
+  Widget button(Widget widget, Function function) => ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          fixedSize: Size(MediaQuery.of(context).size.width * 6 / 7,
+              MediaQuery.of(context).size.height * 1 / 7),
+          backgroundColor: const Color.fromRGBO(170, 25, 52, 1),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(width: 3),
+          ),
+          foregroundColor: Colors.black),
+      onPressed: () => function(),
+      child: widget);
 }
