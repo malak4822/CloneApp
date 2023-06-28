d76a8b850764636b422d384611092d12ed6ff83a
malak4822 <kmalak4822@gmail.com>
2023-06-28 16:01:07 +0200

    smallchanges

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,7 +1,23 @@
-void firstButton() {}
+import 'package:kacapp/main.dart';
+import 'package:kacapp/mywidgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void secondButton() {}
+class Functions {
+  final _url = Uri(scheme: 'tel', path: '999');
 
-void thirdButton() {}
+  Functions();
 
-void fourthButton() {}
+  Future<void> firstButton() async {}
+
+  Future<void> secondButton() async {
+    if (await canLaunchUrl(_url)) {
+      await launchUrl(_url);
+    }
+  }
+
+  void thirdButton() {
+    MyWidgets().toogle();
+  }
+
+  void fourthButton() {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:kacapp/fadeTxt.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:share_plus/share_plus.dart';
+import 'package:kacapp/functions.dart';
+import 'package:kacapp/mywidgets.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Kac App',
-      home: MyHomePage(title: ' Kac App'),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Kac App',
+        home: ChangeNotifierProvider(
+          create: (context) => MyWidgets(),
+          child: Consumer<MyWidgets>(builder: (context, stateProvider, _) {
+            return const MyHomePage(title: ' Kac App');
+          }),
+        ));
   }
 }
 
@@ -29,23 +33,15 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
-  bool canChooseLvl = false;
   final TextStyle aTxtStyle =
       const TextStyle(color: Colors.white, fontSize: 16);
   double lvl = 0.5;
 
-  final Uri url = Uri(scheme: 'tel', path: '999');
-
   String shortenedLvl() {
     double level = lvl * 10;
     return level.toStringAsFixed(1);
-  }
-
-  Future<bool> hideAfter() async {
-    Future.delayed(const Duration(milliseconds: 700));
-    bool visible = !canChooseLvl;
-    return visible;
   }
 
   @override
@@ -69,88 +65,26 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontWeight: FontWeight.w500)),
       ),
       body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            button(Text('ZNAJDŹ PIJĄCEGO W POBLIŻU', style: aTxtStyle), () {}),
-            const Spacer(),
-            button(Text('ZAMÓW KARETKE', style: aTxtStyle), () async {
-              if (await canLaunchUrl(url)) {
-                await launchUrl(url);
-              }
-            }),
-            const Spacer(),
-            button(
-                Column(children: [
-                  AnimatedOpacity(
-                      duration: const Duration(milliseconds: 700),
-                      opacity: !canChooseLvl ? 1 : 0,
-                      child: FutureBuilder(
-                          future: hideAfter(),
-                          builder: (context, snapshot) => Text(
-                              'UDOSTĘPNIJ SWÓJ STAN UPOJENIA',
-                              style: aTxtStyle))),
-                  AnimatedOpacity(
-                      opacity: canChooseLvl ? 1 : 0,
-                      duration: const Duration(milliseconds: 700),
-                      child: Visibility(
-                          visible: canChooseLvl,
-                          child: Slider(
-                            value: lvl,
-                            onChanged: (newValue) {
-                              setState(() {
-                                lvl = newValue;
-                              });
-                            },
-                            activeColor: Colors.white,
-                            inactiveColor: Colors.white.withOpacity(0.5),
-                          ))),
-                  AnimatedOpacity(
-                      opacity: canChooseLvl ? 1 : 0,
-                      duration: const Duration(milliseconds: 700),
-                      child: Expanded(
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                            Text(
-                              "Upodliłem się ${shortenedLvl()}/10",
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                            IconButton(
-                                onPressed: () async {
-                                  await Share.share(
-                                      'Upodliłem się ${shortenedLvl()}/10 ',
-                                      subject: 'Jak Bardzo Się Upodliłem');
-                                },
-                                icon: const Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                ))
-                          ])))
-                ]), () {
-              setState(() {
-                canChooseLvl = !canChooseLvl;
-              });
-            }),
-            const Spacer(),
-            button(Text('ZAMÓW KLINA ON-LINE', style: aTxtStyle), () {}),
-            const Spacer(),
-          ],
-        ),
+        child: Column(children: [
+          const Spacer(),
+          MyWidgets().button(
+              Text('ZNAJDŹ PIJĄCEGO W POBLIŻU', style: aTxtStyle),
+              () => Functions().firstButton(),
+              context),
+          const Spacer(),
+          MyWidgets().button(Text('ZAMÓW KARETKE', style: aTxtStyle),
+              () => Functions().secondButton(), context),
+          const Spacer(),
+          MyWidgets().button(
+              MyWidgets().thirdButtonWidget(lvl, shortenedLvl),
+              () => Functions().thirdButton(),
+              context),
+          const Spacer(),
+          MyWidgets().button(Text('ZAMÓW KLINA ON-LINE', style: aTxtStyle),
+              () => Functions().fourthButton(), context),
+          const Spacer(),
+        ]),
       ),
     );
   }
-
-  Widget button(Widget widget, Function function) => ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          fixedSize: Size(MediaQuery.of(context).size.width * 6 / 7,
-              MediaQuery.of(context).size.height * 1 / 7),
-          backgroundColor: const Color.fromRGBO(170, 25, 52, 1),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-            side: const BorderSide(width: 3),
-          ),
-          foregroundColor: Colors.black),
-      onPressed: () => function(),
-      child: widget);
 }
